41bb3aaf886adf74bad078346324781c96e9af50
Alejandro Sastoque <sastoque42@gmail.com>
2023-02-15 22:05:11 -0500

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://AlejoSasto.github.io/',
+  base:'https://AlejoSasto.github.io/gh-react-vite/',
   plugins: [react()],
 })
